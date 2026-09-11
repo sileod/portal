fcef1151e292e16d8cf16194b27f5ef3fbaa6a7b
sileod <damien.sileo@gmail.com>
2026-09-11 16:50:12 +0200

    Make terminal drag select under tmux mouse mode

--- internal/webui/xterm.go
+++ internal/webui/xterm.go
@@ -5,8 +5,11 @@ import "bytes"
 func init() {
 	IndexHTML = bytes.Replace(IndexHTML,
 		[]byte(`https://cdn.jsdelivr.net/npm/xterm@5.3.0/css/xterm.css`),
-		[]byte(`https://cdnjs.cloudflare.com/ajax/libs/xterm/5.4.0/xterm.css`), 1)
+		[]byte(`https://cdn.jsdelivr.net/npm/@xterm/xterm@6.1.0-beta.303/css/xterm.css`), 1)
 	IndexHTML = bytes.Replace(IndexHTML,
 		[]byte(`https://cdn.jsdelivr.net/npm/xterm@5.3.0/lib/xterm.js`),
-		[]byte(`https://cdnjs.cloudflare.com/ajax/libs/xterm/5.4.0/xterm.js`), 1)
+		[]byte(`https://cdn.jsdelivr.net/npm/@xterm/xterm@6.1.0-beta.303/lib/xterm.js`), 1)
+	IndexHTML = bytes.Replace(IndexHTML,
+		[]byte(`scrollback:10000,theme:`),
+		[]byte(`scrollback:10000,mouseEventsRequireAlt:true,theme:`), 1)
 }
